2131ddcc1a7a37275a9fddcee2ed82d92fdf3c3f
PengJiyuan <731401082@qq.com>
2018-11-02 10:49:37 +0800

    game finish

---
+++ src/utils.js
@@ -0,0 +1,39 @@
+export function addEvent(element, type, handler) {
+  if (element.addEventListener) {
+    element.addEventListener(type, handler, false);
+  } else if (element.attachEvent) {
+    element.attachEvent('on' + type, handler);
+  } else {
+    element['on' + type] = handler;
+  }
+}
+
+export function removeEvent(element, type, handler) {
+  if (element.removeEventListener) {
+    element.removeEventListener(type, handler, false);
+  } else if (element.detachEvent) {
+    element.detachEvent('on' + type, handler);
+  } else {
+    element['on' + type] = null;
+  }
+}
+
+export function getEventPosition(ev) {
+  let x, y;
+  if (ev.touches) {
+	  x = ev.touches[0].pageX;
+    y = ev.touches[0].pageY;
+  } else if (ev.layerX || ev.layerX === 0) {
+    x = ev.layerX;
+    y = ev.layerY;
+  } else if (ev.offsetX || ev.offsetX === 0) {
+    x = ev.offsetX;
+    y = ev.offsetY;
+  }
+  return { x, y };
+}
+
+//获得start到end的随机数
+export function getRandom(start, end) {
+	return Math.round(Math.random() * (end - start) + start);
+}
